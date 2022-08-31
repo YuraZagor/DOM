10c10edd4a2258f8adc1e78bf692ab14fdaa414c
Yuriy Zagorulko <109472016+YuraZagor@users.noreply.github.com>
2022-08-31 15:34:16 +0300

    somehow it works

--- index.js
+++ index.js
@@ -1,61 +1,159 @@
 import { data } from './data.js'  
 
-let newText = data[0].content
+const body = document.querySelector('body')
+let text = data[0].content
 
-function start() {
-	const div = document.createElement('div');
-	div.classList.add('wrapper');
-	const body = document.querySelector('body')
-	body.appendChild(div)
+const div = document.createElement('div')
+div.classList.add('wrapper')
 
-	const main = document.createElement('main')
-	main.classList.add('content')
-	div.appendChild(main)
+body.appendChild(div)
+
+const main = document.createElement('main')
+main.classList.add('main')
+div.appendChild(main)	
+
+const header = document.createElement('header')
+header.classList.add('header')
+main.appendChild(header)
+
+const content = document.createElement('div')
+content.classList.add('content')
+main.appendChild(content)
 
 
-	const contentP = document.createElement('div');
-	contentP.classList.add('content--p')
-	contentP.innerHTML = newText
-	main.appendChild(contentP)
+const footer = document.createElement('footer')
+footer.classList.add('footer')
+main.appendChild(footer)
 
-	const ul = document.createElement('ul')
-	ul.classList.add('sidenav--ul')
-	main.insertAdjacentElement('beforebegin', ul)
 
-	ul.innerHTML = data.map( (item) =>  `
-	<li class="sidenav--li" onclick = 'open(${item.number - 1})'>
-	<img src=${item.img} alt=${item.text} class="li--img" >
-	</li>
-	` ).join(' ')
+content.innerHTML = text
+
+const ul = document.createElement('ul')
+ul.classList.add('sidenav--ul')
+main.insertAdjacentElement('beforebegin', ul)
+
+for (let i=0; i<data.length; i++){
+	let sidenavLi = document.createElement('li')
+	sidenavLi.classList.add('sidenav--li')
+
+	ul.appendChild(sidenavLi)
+	sidenavLi.setAttribute('id', data[i].number-1)
+	const img = document.createElement('img')
+	img.classList.add('li--img')
+	img.setAttribute('src', data[i].img )
+	img.setAttribute('alt', data[i].text )
+	sidenavLi.appendChild(img)
 }
-start()
 
-function open(x) {
-	console.log('open')
-	newText = data[x].content
-	start()
-}
+document.getElementById('0').addEventListener ('click', ()=> {
+	text= data[0].content
+	content.innerHTML = text
+	})
+document.getElementById('1').addEventListener ('click', ()=> {
+	text= data[1].content
+	content.innerHTML = text
+	})
+document.getElementById('2').addEventListener ('click', ()=> {
+	text= data[2].content
+	content.innerHTML = text
+	})
+document.getElementById('3').addEventListener ('click', ()=> {
+	text= data[3].content
+	content.innerHTML = text
+	})
+document.getElementById('4').addEventListener ('click', ()=> {
+	text= data[4].content
+	content.innerHTML = text
+	})
+document.getElementById('5').addEventListener ('click', ()=> {
+	text= data[5].content
+	content.innerHTML = text
+	})
 
 
+
+
+
+// function handleclick {
+// 	text= data[1].content
+// 	content.innerHTML = text
+// }
+
+// function open() {
+// 	text= data[2].content
+// 	content.innerHTML = text
+// 	}
+
+// --------------------------------------------------
+
+// sidenavLi.setAttribute ('onclick', `open(${[i]})`)
+// function open(x) {
+// 	text= data[x].content
+// 	content.innerHTML = text
+
+// 	}
+
+
+// ----------------------------------------------------
+
+// let newText = data[0].content
+
+// function start() {
+// 	const div = document.createElement('div')
+// 	div.classList.add('wrapper')
+// 	const body = document.querySelector('body')
+// 	body.appendChild(div)
+
+// 	const main = document.createElement('main')
+// 	main.classList.add('main')
+// 	div.appendChild(main)
+
+
+// 	const content = document.createElement('div')
+// 	content.classList.add('content')
+// 	content.innerHTML = newText
+// 	main.appendChild(content)
+
+// 	const ul = document.createElement('ul')
+// 	ul.classList.add('sidenav--ul')
+// 	main.insertAdjacentElement('beforebegin', ul)
+
+// 	ul.innerHTML = data.map( (item) =>  `
+// 	<li class="sidenav--li" onclick = 'open(${item.number - 1})'>
+// 	<img src=${item.img} alt=${item.text} class="li--img" >
+// 	</li>
+// 	` ).join(' ')
+// }
+// start()
+
+// function open(x) {
+// 	console.log('open')
+// 	newText = data[x].content
+// 	start()
+// }
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 // import { data } from './data.js'   
 
 // function open(x=0) {
 // 	return document.getElementsByClassName('content--p').textContent = data[x].content
 // }
-// const div = document.createElement('div');
-// div.classList.add('wrapper');
+// const div = document.createElement('div')
+// div.classList.add('wrapper')
 // const body = document.querySelector('body')
 // body.appendChild(div)
 
 // const main = document.createElement('main')
-// main.classList.add('content')
+// main.classList.add('main')
 // div.appendChild(main)
 
-// const contentP = document.createElement('div');
-// contentP.classList.add('content--p')
-// contentP.textContent = open()
-// main.appendChild(contentP)
+// const content = document.createElement('div')
+// content.classList.add('content')
+// content.textContent = open()
+// main.appendChild(content)
 
 // const ul = document.createElement('ul')
 // ul.classList.add('sidenav--ul')
